68b9c3a94bebdb055a9b8defa86fdb1e4e02563e
Davide Liu <41103541+davide97l@users.noreply.github.com>
2022-08-23 15:08:17 +0800

    custom data workflow

--- scripts/process_csv/process_csv.py
+++ scripts/process_csv/process_csv.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--symbol', type=str, default='symbol')
 parser.add_argument('--csv_file', type=str, required=True)
 parser.add_argument('--csv_dir', type=str, required=True)
+parser.add_argument('--limit_stocks', type=int, required=False, default=None)
 args = parser.parse_args()
 
 symbol = args.symbol
@@ -14,6 +15,7 @@ csv_file = args.csv_file
 csv_dir = args.csv_dir
 df = pd.read_csv(csv_file)
 codes = df[symbol].unique()
-for c in tqdm(codes, desc='Creating stock files'):
+limit = args.limit_stocks if args.limit_stocks is not None else len(codes)
+for c in tqdm(codes[:limit], desc='Creating stock files'):
     df_code = df[df[symbol] == c].reset_index(drop=True)
     df.to_csv(os.path.join(csv_dir, c + '.csv'), encoding='utf-8', index=False)
